abbdd975e6f20e7575e83eea1861ce76ab6f6b39
LOGESH-D <kit26.csbs30@gmail.com>
2026-03-16 16:22:23 +0530

    fix for deploy

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,10 +9,16 @@ from app.routes import admin
 load_dotenv()
 
 app = FastAPI(title="Medical Symptom Checker")
+FRONTEND_URL = os.getenv("FRONTEND_URL")
+origins = [
+    FRONTEND_URL,
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("FRONTEND_URL")],
+    allow_origins=[origin for origin in origins if origin],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
